d8a7a8349bf29b8b28795828b77f3a1d77dbaf5b
Clement Dufaure <clementdufaure@hotmail.com>
2020-11-12 14:41:18 +0100

    add cors customization
    Signed-off-by: Clement Dufaure <clementdufaure@hotmail.com>

--- sugoi-api-services/src/main/java/fr/insee/sugoi/services/configuration/CorsConfiguration.java
+++ sugoi-api-services/src/main/java/fr/insee/sugoi/services/configuration/CorsConfiguration.java
@@ -1,20 +1,37 @@
 package fr.insee.sugoi.services.configuration;
 
+import java.util.regex.Pattern;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ConfigurationProperties("fr.insee.sugoi.cors")
 public class CorsConfiguration {
+
+    /**
+     * Pattern of urls accepted by CORS protection
+     */
+    private String pathPattern = "/**";
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping(pathPattern);
             }
         };
+    }
+
+    public String getPathPattern() {
+        return pathPattern;
+    }
+
+    public void setPathPattern(String pathPattern) {
+        this.pathPattern = pathPattern;
     }
 }
