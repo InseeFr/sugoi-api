07cac318b6e0a08e3795ab3ae5c6a0db00747384
Clément Dufaure <45071499+clement-dufaure@users.noreply.github.com>
2020-11-24 11:43:12 +0100

    add security configuration (#8)
    * add security configuration (oidc, basic+ldap, cors)
    
    Signed-off-by: Clement Dufaure <clementdufaure@hotmail.com>

--- sugoi-api-services/src/main/java/fr/insee/sugoi/services/configuration/CorsConfiguration.java
+++ sugoi-api-services/src/main/java/fr/insee/sugoi/services/configuration/CorsConfiguration.java
@@ -1,20 +1,41 @@
 package fr.insee.sugoi.services.configuration;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ConfigurationProperties("fr.insee.sugoi.cors")
 public class CorsConfiguration {
+
+    /**
+     * Pattern of urls accepted by CORS protection
+     */
+    private String PATH_PATTERN = "/**";
+
+    /**
+     * Array of origins allowed to connect
+     */
+    private String[] allowedOrigins = {};
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping(PATH_PATTERN).allowedOrigins(allowedOrigins);
             }
         };
     }
+
+    public String[] getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(String[] allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
 }
