2fec4e08ce63a968e1cd5a3aa9edd79b56ec7a54
Donatien Eneman <donatien.eneman@insee.fr>
2020-12-14 14:05:05 +0100

    first commit
    Signed-off-by: Donatien Eneman <donatien.eneman@insee.fr>

--- sugoi-api-rest-old-services/src/main/java/fr/insee/sugoi/services/controller/ContactDomaineController.java
+++ sugoi-api-rest-old-services/src/main/java/fr/insee/sugoi/services/controller/ContactDomaineController.java
@@ -13,7 +13,8 @@
 */
 package fr.insee.sugoi.services.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,27 +23,175 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.insee.sugoi.converter.mapper.OuganextSugoiMapper;
 import fr.insee.sugoi.converter.ouganext.Contact;
-import fr.insee.sugoi.core.service.UserService;
+import fr.insee.sugoi.converter.ouganext.InfoFormattage;
+import fr.insee.sugoi.converter.ouganext.PasswordChangeRequest;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 
 @RestController
 @RequestMapping("/v1")
-@Tag(name = "Contact")
+@Tags(value = { @Tag(name = "V1"), @Tag(name = "contact") })
 public class ContactDomaineController {
 
-        @Autowired
-        private UserService userService;
+        // @Autowired
+        // private UserService userService;
 
-        @Autowired
-        private OuganextSugoiMapper ouganextSugoiMapper;
+        // @Autowired
+        // private OuganextSugoiMapper ouganextSugoiMapper;
 
         @RequestMapping(method = RequestMethod.PUT, consumes = { MediaType.APPLICATION_XML_VALUE,
                         MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_XML_VALUE,
                                         MediaType.APPLICATION_JSON_VALUE }, path = "/{domaine}/contact/{id}")
-        public ResponseEntity<Contact> createOrModifyContact(Contact contact, @PathVariable("id") String identifiant,
+        public ResponseEntity<?> createOrModifyContact(Contact contact, @PathVariable("id") String identifiant,
                         @PathVariable("domaine") String domaine, @RequestParam("creation") boolean creation) {
                 return null;
         }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}", method = RequestMethod.GET, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> getContactDomaine(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}", method = RequestMethod.DELETE, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> deleteContact(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/password", method = RequestMethod.POST, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> reinitPassword(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine,
+                        @RequestParam("modeEnvoi") List<String> modeEnvoisString, PasswordChangeRequest pcr) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/password/first", method = RequestMethod.POST, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> initPassword(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine,
+                        @RequestParam("modeEnvoi") List<String> modeEnvoisString, PasswordChangeRequest pcr) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/password", method = RequestMethod.PUT, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> changePassword(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine,
+                        @RequestParam("modeEnvoi") List<String> modeEnvoisString, PasswordChangeRequest pcr) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/habilitations", method = RequestMethod.GET, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> getHabilitation(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/habilitations/{application}", method = RequestMethod.PUT, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> addHabilitations(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine, @PathVariable("application") String appName,
+                        @RequestParam("role") List<String> nomRoles) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/habilitations/{application}/{role}", method = RequestMethod.PUT, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> addHabilitationsWithProperty(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine, @PathVariable("application") String appName,
+                        @PathVariable("role") @RequestParam("propriete") List<String> nomRoles) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/habilitations/{application}", method = RequestMethod.DELETE, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> deleteHabilitations(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine, @PathVariable("application") String appName,
+                        @RequestParam("role") List<String> nomRoles) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/habilitations/{application}/{role}", method = RequestMethod.DELETE, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> deleteHabilitationsWithProperty(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine, @PathVariable("application") String appName,
+                        @PathVariable("role") String nomRole, @RequestParam("propriete") List<String> proprietes) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/login", method = RequestMethod.POST, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> envoiLogin(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine,
+                        @RequestParam("modeEnvoi") List<String> modeEnvoiStrings, InfoFormattage infoEnvoi) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/groupes", method = RequestMethod.GET, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> getGroups(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/groupes/{nomappli}/{nomgroupe}", method = RequestMethod.PUT, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> addToGroup(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine, @PathVariable("nomappli") String nomAppli,
+                        @PathVariable("nomgroupe") String nomGroupe) {
+                return null;
+        }
+
+        @RequestMapping(value = "{domaine}/contact/{id}/groupes/{nomappli}/{nomgroupe}", method = RequestMethod.DELETE, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> removeFromGroups(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine, @PathVariable("nomappli") String nomAppli,
+                        @PathVariable("nomgroupe") String nomGroupe) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/inseeroles", method = RequestMethod.GET, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> getInseeRoles(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/inseeroles/{inseerole}", method = RequestMethod.PUT, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> addInseeRoles(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine, @PathVariable("inseerole") String inseeRole) {
+                return null;
+        }
+
+        @RequestMapping(value = "/{domaine}/contact/{id}/inseeroles/{inseerole}", method = RequestMethod.DELETE, consumes = {
+                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                                        MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+        public ResponseEntity<?> removeInseeRole(@PathVariable("id") String identifiant,
+                        @PathVariable("domaine") String domaine, @PathVariable("inseerole") String inseeRole) {
+                return null;
+        }
+
 }
--- sugoi-api-rest-old-services/src/main/java/fr/insee/sugoi/services/controller/ContactsDomaineController.java
+++ sugoi-api-rest-old-services/src/main/java/fr/insee/sugoi/services/controller/ContactsDomaineController.java
@@ -1,5 +1,62 @@
 package fr.insee.sugoi.services.controller;
 
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.insee.sugoi.converter.ouganext.Contact;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
+
+@RestController
+@RequestMapping("/v1/")
+@Tags(value = { @Tag(name = "V1"), @Tag(name = "contacts") })
 public class ContactsDomaineController {
 
+    @RequestMapping(value = "/{domaine}/contacts", method = RequestMethod.POST, consumes = {
+            MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                    MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<?> createContactIdsentifiant(@PathVariable("domaine") String domaine,
+            @RequestHeader("Slug") String slug, Contact contact) {
+        return null;
+    }
+
+    @RequestMapping(value = "/{domaine}/contacts", method = RequestMethod.GET, consumes = {
+            MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                    MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<?> getContacts(@PathVariable("domaine") String domaine,
+            @RequestParam("identifiant") String identifiant, @RequestParam("nomCommun") String nomCommun,
+            @RequestParam("description") String description, @RequestParam("organisationId") String organisationId,
+            @RequestParam("mail") String mail, @RequestParam(name = "size", defaultValue = "20") int size,
+            @RequestParam("start") int offset, @RequestParam("searchCookie") String searchCookie,
+            @RequestParam(name = "typeRecherche", defaultValue = "et") String typeRecherche,
+            @RequestParam("habilitation") List<String> habilitations, @RequestParam("application") String application,
+            @RequestParam("role") String role, @RequestParam("rolePropriete") String rolePropriete,
+            @RequestParam(name = "body", defaultValue = "false") boolean resultatsDansBody,
+            @RequestParam(name = "idOnly", defaultValue = "false") boolean identifiantsSeuls,
+            @RequestParam("certificat") String certificat) {
+        return null;
+    }
+
+    @RequestMapping(value = "/{domaine}/contacts/certificat/{id}", method = RequestMethod.GET, consumes = {
+            MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                    MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<?> getAPartirDeIdCertificat(@PathVariable("domaine") String domaine,
+            @PathVariable("id") String id) {
+        return null;
+    }
+
+    @RequestMapping(value = "/{domaine}/contacts/size", method = RequestMethod.GET, consumes = {
+            MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+                    MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<?> getContactsSize(@PathVariable("domaine") String domaine) {
+        return null;
+    }
 }
