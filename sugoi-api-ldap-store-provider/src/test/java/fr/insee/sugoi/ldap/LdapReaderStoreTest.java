a1c0384fcdd2a04a7edc7438e75401d80384067e
CChemin <cecile.chemin@insee.fr>
2020-12-28 19:52:03 +0100

    init ldapstore tests
    Signed-off-by: CChemin <cecile.chemin@insee.fr>

---
+++ sugoi-api-ldap-store-provider/src/test/java/fr/insee/sugoi/ldap/LdapReaderStoreTest.java
@@ -0,0 +1,58 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package fr.insee.sugoi.ldap;
+
+import fr.insee.sugoi.model.Realm;
+import fr.insee.sugoi.model.UserStorage;
+import fr.insee.sugoi.store.ldap.LdapStoreBeans;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest(classes = {EmbeddedLdapAutoConfiguration.class, LdapStoreBeans.class})
+@TestPropertySource(locations = "classpath:/application.properties")
+public class LdapReaderStoreTest {
+
+  @Value("${fr.insee.sugoi.ldap.default.organizationsource:}")
+  private String organizationSource;
+
+  @Value("${fr.insee.sugoi.ldap.default.appsource:}")
+  private String appSource;
+
+  @Value("${fr.insee.sugoi.ldap.default.usersource:}")
+  private String userSource;
+
+  @Bean
+  public UserStorage userStorage() {
+    UserStorage us = new UserStorage();
+    us.setOrganizationSource(organizationSource);
+    us.setUserSource(userSource);
+    us.setName("default");
+    return us;
+  }
+
+  @Bean(name = "Realm")
+  public Realm realm() {
+    Realm realm = new Realm();
+    realm.setName("domaine1");
+    realm.setUrl("localhost");
+    return realm;
+  }
+
+  @Autowired ApplicationContext context;
+}
