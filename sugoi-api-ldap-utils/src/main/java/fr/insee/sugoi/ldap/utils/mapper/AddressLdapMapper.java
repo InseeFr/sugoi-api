fc2f7226bf6927e73ce9b308156be17402d54e53
CChemin <cecile.chemin@insee.fr>
2021-02-16 19:04:58 +0100

    Fix update address with null value
    Signed-off-by: CChemin <cecile.chemin@insee.fr>

--- sugoi-api-ldap-utils/src/main/java/fr/insee/sugoi/ldap/utils/mapper/AddressLdapMapper.java
+++ sugoi-api-ldap-utils/src/main/java/fr/insee/sugoi/ldap/utils/mapper/AddressLdapMapper.java
@@ -29,52 +29,25 @@ public class AddressLdapMapper {
 
   public static Map<String, String> mapFromSearchEntry(SearchResultEntry searchResultEntry) {
     Map<String, String> address = new HashMap<>();
-    address.put(
-        "Ligne1", searchResultEntry.getAttributeValue("inseeAdressePostaleCorrespondantLigne1"));
-    address.put(
-        "Ligne2", searchResultEntry.getAttributeValue("inseeAdressePostaleCorrespondantLigne2"));
-    address.put(
-        "Ligne3", searchResultEntry.getAttributeValue("inseeAdressePostaleCorrespondantLigne3"));
-    address.put(
-        "Ligne4", searchResultEntry.getAttributeValue("inseeAdressePostaleCorrespondantLigne4"));
-    address.put(
-        "Ligne5", searchResultEntry.getAttributeValue("inseeAdressePostaleCorrespondantLigne5"));
-    address.put(
-        "Ligne6", searchResultEntry.getAttributeValue("inseeAdressePostaleCorrespondantLigne6"));
-    address.put(
-        "Ligne7", searchResultEntry.getAttributeValue("inseeAdressePostaleCorrespondantLigne7"));
+    for (int i = 1; i < 8; i++) {
+      address.put(
+          "Ligne" + String.valueOf(i),
+          searchResultEntry.getAttributeValue(
+              "inseeAdressePostaleCorrespondantLigne" + String.valueOf(i)));
+    }
     return address;
   }
 
   public static List<Attribute> mapToAttributes(Map<String, String> address) {
     List<Attribute> attributes = new ArrayList<>();
-    if (address.containsKey("Ligne1")) {
-      attributes.add(
-          new Attribute("inseeAdressePostaleCorrespondantLigne1", address.get("Ligne1")));
-    }
-    if (address.containsKey("Ligne2")) {
-      attributes.add(
-          new Attribute("inseeAdressePostaleCorrespondantLigne2", address.get("Ligne2")));
-    }
-    if (address.containsKey("Ligne3")) {
-      attributes.add(
-          new Attribute("inseeAdressePostaleCorrespondantLigne3", address.get("Ligne3")));
-    }
-    if (address.containsKey("Ligne4")) {
-      attributes.add(
-          new Attribute("inseeAdressePostaleCorrespondantLigne4", address.get("Ligne4")));
-    }
-    if (address.containsKey("Ligne5")) {
-      attributes.add(
-          new Attribute("inseeAdressePostaleCorrespondantLigne5", address.get("Ligne5")));
-    }
-    if (address.containsKey("Ligne6")) {
-      attributes.add(
-          new Attribute("inseeAdressePostaleCorrespondantLigne6", address.get("Ligne6")));
-    }
-    if (address.containsKey("Ligne7")) {
-      attributes.add(
-          new Attribute("inseeAdressePostaleCorrespondantLigne7", address.get("Ligne7")));
+    for (int i = 1; i < 8; i++) {
+      if (address.containsKey("Ligne" + String.valueOf(i))
+          && address.get("Ligne" + String.valueOf(i)) != null) {
+        attributes.add(
+            new Attribute(
+                "inseeAdressePostaleCorrespondantLigne" + String.valueOf(i),
+                address.get("Ligne" + String.valueOf(i))));
+      }
     }
     attributes.add(
         new Attribute(
