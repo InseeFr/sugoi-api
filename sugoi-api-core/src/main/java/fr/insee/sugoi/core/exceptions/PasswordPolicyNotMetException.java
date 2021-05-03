d972673525f1249e11c37646cfb664b222c96844
CChemin <cecile.chemin@insee.fr>
2021-05-03 21:50:46 +0200

    Fix [310] :bug: Some exceptions are not caugth by SugoiAdviceController
    Signed-off-by: CChemin <cecile.chemin@insee.fr>

--- sugoi-api-core/src/main/java/fr/insee/sugoi/core/exceptions/PasswordPolicyNotMetException.java
+++ sugoi-api-core/src/main/java/fr/insee/sugoi/core/exceptions/PasswordPolicyNotMetException.java
@@ -13,10 +13,6 @@
 */
 package fr.insee.sugoi.core.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.CONFLICT)
 public class PasswordPolicyNotMetException extends RuntimeException {
 
   private static final long serialVersionUID = 1L;
