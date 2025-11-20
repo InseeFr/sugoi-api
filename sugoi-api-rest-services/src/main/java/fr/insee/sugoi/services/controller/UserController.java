e22d6e1e594338441c4031e044952c262a2f123a
Olivier Ricciardi <olivier.ricciardi@insee.fr>
2025-11-20 18:31:51 +0100

    [UP] rajout @P(realm) et storage pour createUsers
    Signed-off-by: Olivier Ricciardi <olivier.ricciardi@insee.fr>

--- sugoi-api-rest-services/src/main/java/fr/insee/sugoi/services/controller/UserController.java
+++ sugoi-api-rest-services/src/main/java/fr/insee/sugoi/services/controller/UserController.java
@@ -58,6 +58,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -324,11 +325,13 @@ public class UserController {
               description = "Name of the realm where the operation will be made",
               required = true)
           @PathVariable("realm")
+          @P("realm")
           String realm,
       @Parameter(
               description = "Name of the userStorage where the operation will be made",
               required = false)
           @PathVariable(name = "storage", required = false)
+          @P("storage")
           String storage,
       @Parameter(description = "Allowed asynchronous request", required = false)
           @RequestHeader(name = "X-SUGOI-ASYNCHRONOUS-ALLOWED-REQUEST", defaultValue = "false")
